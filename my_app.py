b91bd1167d3b8a898552a408ac220f840d9716b5
unknown <randu8578@gmail.com>
2024-09-12 15:16:05 +0700

    Perubahan code my_app dan intr

--- my_app.py
+++ my_app.py
@@ -1,1 +1,45 @@
-# write the code for main app and first screen
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
+
+from instr import *
+from second_win import *
+
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
+    def initUI(self):
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.button = QPushButton(txt_next)
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.button, alignment = Qt.AlignCenter)         
+        self.setLayout(self.layout_line)
+    
+    def connects(self):
+        self.button.clicked.connect(self.next_click)
+        
+    def next_click(self):
+        self.hide()
+
+
+app = QApplication([])
+mw = MainWin()
+app.exec_()
+
+
